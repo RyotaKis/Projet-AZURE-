cb2f9977bc24796da79a0c622c830db403fc67e3
RyotaKis <ryotakis@github.com>
2026-04-28 03:34:06 +0200

    Configure Swagger for NestJS microservices

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('AZURE+ API Gateway')
+    .setDescription('Le point d\\'entrée unique pour le système de détection de fraude AZURE+')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- transaction-service/src/main.ts
+++ transaction-service/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('AZURE+ Transaction Service (Core Ledger)')
+    .setDescription('Microservice de gestion des comptes et de l\\'historique des transactions')
+    .setVersion('1.0')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(process.env.PORT ?? 4000); // Port 4000 pour le Transaction Service
 }
 bootstrap();
